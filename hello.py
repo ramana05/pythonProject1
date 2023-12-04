f622565dfd789334113536f037a282322051b849
Ram Mantravadi <mantravadi.ramana@gmail.com>
2023-12-03 23:12:12 -0500

    Update hello.py
    Bad code for arbitrary code execution

--- hello.py
+++ hello.py
@@ -2,6 +2,13 @@ import os
 from flask import request
 import sqlalchemy
 
+# example code snippet py_vuln00: Arbitrary Code Execution:
+compute_user_input = input('\nType something here to compute: ')
+if not compute_user_input:
+        print ("No input")
+else:
+        print ("Result: ", eval(compute_user_input))
+
 print('Hello world is there or not ')
 def get_users():
     user = request.args["user"]
